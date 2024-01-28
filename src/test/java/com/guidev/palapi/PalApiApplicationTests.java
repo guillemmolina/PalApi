a0a5f13011b6e7ac997425e6731d3f10ab17adc2
guillemmolina <108735005+guillemmolina@users.noreply.github.com>
2024-01-28 19:59:40 +0100

    first commit

---
+++ src/test/java/com/guidev/palapi/PalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guidev.palapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PalApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
